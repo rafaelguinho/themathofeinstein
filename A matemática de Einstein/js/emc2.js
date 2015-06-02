3f2df06f38ddeae6d5d5f5eba8d3d92747b864bc
Rafael Guinho <rafaelguinho@outlook.com>
2015-06-02 05:54:04 -0300

    Subindo o código do projeto

---
+++ A matemática de Einstein/js/emc2.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+
+    var VELOCIDADE_LUZ_VACUO_KM_S = 300000;
+
+    document.getElementById('btnCalcular').addEventListener('click', function (e) {
+
+        e.preventDefault();
+
+        var massa = document.getElementById('txtMassa').value;
+
+        var energia = calcularEnergia(massa, VELOCIDADE_LUZ_VACUO_KM_S);
+
+        exibirValorEnergia(energia);
+
+    });
+
+    function calcularEnergia(massa, velocidadeLuz) {
+        if(massa == '') return 0;
+        return massa * (velocidadeLuz * velocidadeLuz);
+    }
+
+    function exibirValorEnergia(energia) {
+        document.getElementById('lblResultadoEnergia').innerHTML = energia + '<small> joules</small>';
+    }
+};
